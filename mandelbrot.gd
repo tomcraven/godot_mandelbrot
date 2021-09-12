3575a07d6b26431e3e1a3735a2483fc3dd5fdfc3
Tom Craven <tom.cass.craven@gmail.com>
2021-09-12 18:51:09 +0100

    initial commit

--- mandelbrot.gd
+++ mandelbrot.gd
@@ -2,23 +2,21 @@ extends Control
 
 onready var mat: Material = $ViewportContainer.get_material()
 onready var initial_pos: Vector2 = mat.get_shader_param("position")
-onready var initial_zoom: float = mat.get_shader_param("zoom")
-onready var initial_detail: int = mat.get_shader_param("detail")
+onready var initial_re_bounds: Vector2 = mat.get_shader_param("re_bounds")
+onready var initial_im_bounds: Vector2 = mat.get_shader_param("im_bounds")
 
-func _ready():
-	mat.set_shader_param("palette", $ViewportContainer/Viewport/TextureRect.texture)
+var zoom = 1.0
 
 func _process(delta: float):
 	var pos: Vector2 = mat.get_shader_param("position")
-	var zoom: float = mat.get_shader_param("zoom")
-	var detail: int = mat.get_shader_param("detail")
+	var re_bounds: Vector2 = mat.get_shader_param("re_bounds")
+	var im_bounds: Vector2 = mat.get_shader_param("im_bounds")
 	
 	var direction = Vector2.ZERO
 	if Input.is_action_pressed("ui_left"):
 		direction.x -= 1.0
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1.0
-
 	if Input.is_action_pressed("ui_up"):
 		direction.y += 1.0
 	if Input.is_action_pressed("ui_down"):
@@ -28,20 +26,22 @@ func _process(delta: float):
 	pos += direction
 	
 	if Input.is_action_pressed("zoom_in"):
-		zoom += 1.0 * delta * zoom
-	if Input.is_action_pressed("zoom_out"):
 		zoom -= 1.0 * delta * zoom
+	if Input.is_action_pressed("zoom_out"):
+		zoom += 1.0 * delta * zoom
 
-	if Input.is_action_pressed("detail_up"):
-		detail += 1.0
-	if Input.is_action_pressed("detail_down"):
-		detail -= 1.0
+	# https://stackoverflow.com/a/60815690
+	var re_range = 3.5 * zoom;
+	var im_range = 2.0 * zoom;
+	re_bounds = Vector2(0.5 - re_range / 2.0, 0.5 + re_range / 2.0)
+	im_bounds = Vector2(0.5 - im_range / 2.0, 0.5 + im_range / 2.0)
 
 	if Input.is_action_just_pressed("reset"):
 		pos = initial_pos
-		zoom = initial_zoom
-		detail = initial_detail
-		
+		zoom = 1.0
+		re_bounds = initial_re_bounds
+		im_bounds = initial_im_bounds
+
 	mat.set_shader_param("position", pos)
-	mat.set_shader_param("zoom", zoom)
-	mat.set_shader_param("detail", detail)
+	mat.set_shader_param("re_bounds", re_bounds)
+	mat.set_shader_param("im_bounds", im_bounds)
